4ebcd09abc0b3ec3154d10ad839b61f2060674fc
orianaavasquez <l24141301@queretaro.tecnm.mx>
2024-10-22 12:58:44 -0600

    FUNCIONNUMEROMINIMO

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ def hello_world():
     print("Hello World")
     return True
 
+def get_minimum(my_list):
+    min_value=my_list[0]
+    for element in my_list:
+        if min_value >= element:
+            min_value =element
+    return min_value
+
+
+
 hello_world()
--- tests.py
+++ tests.py
@@ -1,7 +1,17 @@
 import pytest
+from numpy.ma.testutils import assert_almost_equal
+
+
 import main as mn
-from main import hello_world
 
 
 def test_hello_world():
     assert mn.hello_world() is True
+
+def test_get_minimum_1():
+    resultado = mn.get_minimum([3,1,3])
+    assert resultado is 1
+
+def test_get_minimum_2():
+    resultado = mn.get_minimum([3.0,1,-9.2,0])
+    assert_almost_equal(resultado, -9.2, decimal=1)
